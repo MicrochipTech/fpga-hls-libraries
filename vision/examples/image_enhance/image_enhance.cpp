011909af38805fb1fc6dfb9ed33288d5d1a78211
Manuel Saldana <manuel.saldana@microchip.com>
2024-05-27 16:27:43 -0400

    Added GammaCorrection and ImageEnhance functions
    - Also added the example designs for testing each function.
    - For the GammaCorrection function, a table generator has been added. This is a temporary solution until we can generate tables at compile time.
    - Also updated the .gitignore file

--- vision/examples/image_enhance/image_enhance.cpp
+++ vision/examples/image_enhance/image_enhance.cpp
@@ -1,5 +1,5 @@
+#include "opencv2/imgproc.hpp"
 #include "vision.hpp"
-#include <opencv2/opencv.hpp>
 
 using namespace hls;
 using cv::Mat;
@@ -20,17 +20,16 @@ template <
 void ImageEnhanceWrapper(
     vision::Img<PIXEL_T, H, W, STORAGE, NPPC> &ImgIn,
     vision::Img<PIXEL_T, H, W, STORAGE, NPPC> &ImgOut,
-    ap_uint<8> b_factor,
-    ap_uint<8> g_factor,
-    ap_uint<8> r_factor,
+    ap_uint<8> b_const,
+    ap_uint<8> g_const,
+    ap_uint<8> r_const,
     ap_int<10> brightness
 ) {
     #pragma HLS function top
-    vision::ImageEnhance(ImgIn, ImgOut, b_factor, g_factor, r_factor, brightness);
+    vision::ImageEnhance(ImgIn, ImgOut, b_const, g_const, r_const, brightness);
 }
 
-int main(int argc, char* argv[]) {
-
+int main() {
     std::string INPUT_IMAGE=argv[1];
 
     Mat BGRInMat = cv::imread(INPUT_IMAGE, cv::IMREAD_COLOR);
@@ -38,7 +37,6 @@ int main(int argc, char* argv[]) {
         printf("Error loading the file: %s\n", INPUT_IMAGE.c_str());
         exit(0);
     }
-    
     Mat RGBMat;
     // ImageEnhance uses RGB format, but OpenCV uses BGR to read from files
     cv::cvtColor(BGRInMat, RGBMat, cv::COLOR_BGR2RGB);
@@ -49,16 +47,15 @@ int main(int argc, char* argv[]) {
 
     // These numbers are intentionally random to try different factors and mess 
     // around with the original color of the image. 
-    ap_uint<8> r_factor = 1;        // red factor = 256/32 = 8
-    ap_uint<8> g_factor = 32;       // green factor = 32/32 = 1
-    ap_uint<8> b_factor = 255;      // blue factor = 256/32 = 0.031
-    ap_int<10> brightness = -60;    // reduce the brightness
+    ap_uint<8> r_const = 255;   // red factor = 256/32 = 8
+    ap_uint<8> g_const = 32;    // green factor = 32/32 = 1
+    ap_uint<8> b_const = 1;     // blue factor = 256/32 = 0.031
+    ap_int<10> brightness = -60; // reduce the brightness
 
-    ImageEnhanceWrapper(InImg, OutImg, b_factor, g_factor, r_factor, brightness);
+    ImageEnhanceWrapper(InImg, OutImg, b_const, g_const, r_const, brightness);
 
     cv::Mat HlsOutMat, HlsOutMatBGR;
     convertToCvMat(OutImg, HlsOutMat);
-    
     // ImageEnhance uses RGB format, but OpenCV uses BGR to write to files
     cv::cvtColor(HlsOutMat, HlsOutMatBGR, cv::COLOR_RGB2BGR);
     cv::imwrite("hls_out.png", HlsOutMatBGR);
