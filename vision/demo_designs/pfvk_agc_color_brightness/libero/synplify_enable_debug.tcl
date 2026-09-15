113ba89ac80f88a08d38e807b356f9c41eb65954
Elizabeth JT <elizabeth.jungtorres@microchip.com>
2026-09-15 14:01:31 -0400

    extra files removed for clearer comparison

--- vision/demo_designs/pfvk_agc_color_brightness/libero/synplify_enable_debug.tcl
+++
@@ -1,4 +0,0 @@
-project -load ./vision_pipeline/synthesis/VIDEO_KIT_TOP_syn.prj
-set_option -identify_debug_mode 1
-set_option -srs_instrumentation 1
-project -save ./vision_pipeline/synthesis/VIDEO_KIT_TOP_syn.prj
